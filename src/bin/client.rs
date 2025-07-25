d41cdecd38faf2bd177ee6a37b7a089d10b29836
EPICAano <103696611+EPICAano@users.noreply.github.com>
2025-07-25 15:47:56 +0200

    Initial commit

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -7,7 +7,7 @@ async fn main() -> anyhow::Result<()> {
     let url = url::Url::parse("ws://127.0.0.1:8081")?;
     let (mut ws_stream, _) = connect_async(url).await?;
 
-    ws_stream.send(Message::Text("Hello WebSocket!".to_string())).await?;
+    ws_stream.send(Message::Text("Hello WebSocket".to_string())).await?;
     if let Some(Ok(msg)) = ws_stream.next().await {
         println!("Réponse : {}", msg);
     }
